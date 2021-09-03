d3b3ed21dd1bb185a135f5e2196cfb103cd9f6b6
robert-rdl <88783977+robert-rdl@users.noreply.github.com>
2021-09-03 23:31:23 +0100

    Adding logging to script

--- scripts/build-latest-reticulum.sh
+++ scripts/build-latest-reticulum.sh
@@ -1,13 +1,18 @@
 export HAB_ORIGIN=raspberry-dream-labs
 export HAB_BLDR_URL=https://bldr.biome.sh
 
+echo "Getting plans"
 git clone https://github.com/robert-rdl/biome-habitats.git
 
+echo "changing directories"
 cd ./biome-habitats/plans
 
+echo "packaging reticulum"
 bio pkg build ./reticulum
 
+echo "uploading reticulum"
 bio pkg upload "$(ls -t /hab/cache/artifacts/raspberry-dream-labs-reticulum-1.0.1* | head -n1)"
 
+echo "promoting reticulum"
 bio pkg promote raspberry-dream-labs/reticulum/1.0.1/"$(ls -t /hab/cache/artifacts/raspberry-dream-labs-reticulum-1.0.1* | head -n1 | sed 's|.*reticulum\-1\.0\.1\-||g' | sed 's|\-.*||g')" stable
 
